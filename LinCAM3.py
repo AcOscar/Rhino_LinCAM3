bd50a5f5548bd7dc6193189681b278bb3dd0cafe
BslOscar <holger.rasch@gmail.com>
2022-10-11 09:46:38 +0200

    add copyright

--- LinCAM3.py
+++ LinCAM3.py
@@ -3,6 +3,7 @@
 #
 # Copyright 2015,2016,2017,2018 Daniel Fernandez MD (daniel@dfmd.mx), Saul Pilatowsky C (saul@dfmd.mx) 
 # distributed by www.ingenierialinarand.com
+# Copyright 2022 Holger Rasch (h.rasch@christgantenbein.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
